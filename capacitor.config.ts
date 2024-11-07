04f3550aa3be783400d4164d5797011892aaffeb
Carlos <carlosiravare@gmail.com>
2024-11-07 15:18:44 -0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RPGWorkout',
+  webDir: 'dist'
+};
+
+export default config;
